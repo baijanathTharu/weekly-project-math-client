655c78fdae395f2e393a754fc851039e4b2248ad
baijanathTharu <bnchaudhary.tharu@gmail.com>
2020-10-21 19:33:38 +0545

    Fix navbar and drawer button

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
+  const [stick, setStick] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 150) {
+        setStick(true);
+      } else {
+        setStick(false);
+      }
+    });
+  });
+
   return (
-    <div className={styles.Navbar}>
+    <div className={`${styles.Navbar} ${stick ? styles.sticky : null}`}>
       <Link to="/">
         <h1>MatheGamics</h1>
       </Link>

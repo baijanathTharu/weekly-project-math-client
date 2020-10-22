e24ede0dfba97539d4b346f8547a28205fe5c2db
baijanathTharu <bnchaudhary.tharu@gmail.com>
2020-10-22 14:53:37 +0545

    Add set up for multiplayer

--- src/components/Multiplayer/Multiplayer.js
+++ src/components/Multiplayer/Multiplayer.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
-import Layoutplayer from "../Layoutplayer/Layoutplayer";
+import Layout from "../Layout/Layout";
+import styles from "./Multiplayer.module.css";
+import { MdClear, MdMenu } from "react-icons/md";
 
 const Multiplayer = ({ user }) => {
+  const [show, setShow] = useState(false);
   return (
-    <>
+    <Layout>
       {user ? null : <Redirect to="/" />}
-      <Layoutplayer title="Multi Player" />;
-    </>
+      <div className={styles.Layoutplayer}>
+        <div
+          className={
+            show
+              ? [styles.Leftside, styles.show].join(" ")
+              : [styles.Leftside, styles.hide].join(" ")
+          }
+        >
+          <h3>Left side</h3>
+        </div>
+        <div className={styles.Main}>
+          <h3>Main</h3>
+        </div>
+        <div className={styles.Button} onClick={() => setShow(!show)}>
+          {show ? <MdClear /> : <MdMenu />}
+        </div>
+      </div>
+    </Layout>
   );
 };
 
